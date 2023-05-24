dc71477251b0a62c1eaa006befaa3961eb2c5b55
chaneldevelops <chanel.developsdesigns@gmail.com>
2023-05-24 16:30:04 -0400

    Added collection to App.js
    - created an unsubscribe function to prevent memory leaks
    - added a Snapshot to update changes to local

--- App.jsx
+++ App.jsx
@@ -3,6 +3,10 @@ import Sidebar from "./components/Sidebar"
 import Editor from "./components/Editor"
 import Split from "react-split"
 import { nanoid } from "nanoid"
+//Allows to listen to changes in firestore database then make changes in local code
+// For ex if I delete a note it will alert Snapshot and update the changes locally in the callback function
+import { onSnapshot } from "firebase/firestore" 
+import { notesCollection } from "./firebase"
 
 export default function App() {
     const [notes, setNotes] = React.useState(
@@ -17,8 +21,12 @@ export default function App() {
         || notes[0]
 
     React.useEffect(() => {
-        localStorage.setItem("notes", JSON.stringify(notes))
-    }, [notes])
+        //Only want to setup when mounts
+        //Going to store in a bariable to prevent memory link & give React the ability to unlink itself from the listener
+        const unsubscribe = onSnapshot(notesCollection, function(snapshot) { // This will give us the most updated notes from notesCollection
+        })
+        return unsubscribe //React will call to clean up side effects
+    }, [])
 
     function createNewNote() {
         const newNote = {
